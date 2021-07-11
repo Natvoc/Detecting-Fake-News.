cc7f427760b82d7d84d19eef195cb4daf6445e6f
Natyari Vargas Oconitrillo <naryari@gmail.com>
2021-07-10 20:30:41 -0600

    Actualización
    Accuracy

--- main.py
+++ main.py
@@ -17,3 +17,23 @@ df.head()
 #obtenemos los labels del DF
 labels=df.label
 labels.head()
+
+#Dividir el conjunto de datos
+x_train,x_test,y_train,y_test=train_test_split(df['text'], labels, test_size=0.2, random_state=7)
+
+#Inicializar TfidfVectorizer
+tfidf_vectorizer=TfidfVectorizer(stop_words='english', max_df=0.7)
+
+# Ajustar y transformar
+tfidf_train=tfidf_vectorizer.fit_transform(x_train)
+tfidf_test=tfidf_vectorizer.transform(x_test)
+
+#Inicializar el PassiveAggressiveClassifier
+pac=PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train,y_train)
+PassiveAggressiveClassifier(max_iter=50)
+
+#Predecir en calculo de Accuracy
+y_pred=pac.predict(tfidf_test)
+score=accuracy_score(y_test,y_pred)
+print(f'Accuracy: {round(score*100,2)}%')
