e5a9995083b8650c018c83746faef513568bfcf8
Natyari Vargas Oconitrillo <naryari@gmail.com>
2021-07-10 20:04:54 -0600

    Actualización
    Limpieza de datos

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
+
+# lectura de datos
+df=pd.read_csv('news.csv')
+
+#obtenemos el shape y head
+df.shape
+df.head()
+
+#obtenemos los labels del DF
+labels=df.label
+labels.head()
