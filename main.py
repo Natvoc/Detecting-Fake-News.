9518fbf85ecfdc6fdf96ef3c4f8e9e5267858ea1
Natyari Vargas Oconitrillo <naryari@gmail.com>
2021-07-10 19:49:27 -0600

    Archivo de ejecución
    Archivo principal para ejecución

---
+++ main.py
@@ -0,0 +1,8 @@
+#Dependencias necesarias
+import numpy as np
+import pandas as pd
+import itertools
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix
